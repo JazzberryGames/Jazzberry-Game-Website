2139d22c265023980de65ced862c180cef15cbf6
atav32 <tangerinewhite32@gmail.com>
2014-11-28 01:01:38 -0500

    adding grunt-aws-s3 task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,10 @@ module.exports = function(grunt) {
 
   // Load Grunt configs
   var path = require('path');
+
   require('load-grunt-config')(grunt, {
     configPath: path.join(process.cwd(), 'grunt_modules'),
   });
-
+  grunt.log.writeln(process.env.AWS_ACCESS_KEY_ID);
+  grunt.log.writeln(process.env.AWS_SECRET_ACCESS_KEY);
 };
